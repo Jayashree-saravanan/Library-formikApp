428a23e0c97d1bc11e5e3c6ee7eedbb41a6f28b8
jayashree <95606189+Jayashree-saravanan@users.noreply.github.com>
2023-04-30 21:44:59 +0200

    final

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+export function Home() {
+  return (
+    <div className="home">
+      <div className="first-div">
+        <h1 className="heading">Welcome to the Library</h1>
+      </div>
+      <div className="footer">
+        <h3></h3>
+      </div>
+    </div>
+  );
+}
